e3dbfd70123e8884b1b3485b9d8b729f129976f4
JunHui Lee <ohenwkgdj@gmail.com>
2015-06-12 18:41:33 +0900

    Change server response contents to json

--- Harmony/src/main/java/kr/mju/tonic/WorkspaceController.java
+++ Harmony/src/main/java/kr/mju/tonic/WorkspaceController.java
@@ -1,11 +1,13 @@
 package kr.mju.tonic;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.mju.tonic.service.WorkspaceService;
+import kr.mju.tonic.webhelper.Defines;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.gson.JsonObject;
 
 @Controller
 public class WorkspaceController {
@@ -25,15 +29,18 @@ public class WorkspaceController {
 	}
 	
 	@RequestMapping(value = "workspace", method = RequestMethod.GET)
-	public String workspace(HttpServletRequest request, Model model) {
-		long id;
+	public void workspace(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		long id = 0;
 		try {
 			id = Long.parseLong(request.getParameter("id"));
-		} catch (Exception e) {e.printStackTrace(); return "redirect:/";}
+		} catch (Exception e) {e.printStackTrace();}
 
-		model.addAttribute("workspace_id", id);
-		model.addAttribute("savedState", service.getPuzzleSet(id));
-		return "main";
+		JsonObject json = new JsonObject();
+		json.addProperty("workspace_id", id);
+		json.addProperty("savedState", service.getPuzzleSet(id));
+		
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.getWriter().write(json.toString());
 	}
 	
 	@RequestMapping(value = "saveWorkspace", method = RequestMethod.POST)
@@ -43,5 +50,20 @@ public class WorkspaceController {
 		System.out.println(state);
 		service.updatePuzzleSet(id, state);
 		response.getWriter().println("fine");
+	}
+	
+	@RequestMapping(value = "getMusicList", method = RequestMethod.GET)
+	public void getMusicList(HttpServletResponse response) throws IOException {
+		File audioDir = new File(Defines.LOCAL_WEBAPP_ROOT_PATH, "resources/audio");
+		JsonObject json = new JsonObject();
+		
+		for (File e : audioDir.listFiles()) {
+			String curName = e.getName();
+			int id = Integer.parseInt(curName.substring(curName.lastIndexOf("_")+1, curName.lastIndexOf(".")));
+			json.addProperty(id+"", curName);	
+		}
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setCharacterEncoding("utf-8");
+		response.getWriter().write(json.toString());
 	}
 }
